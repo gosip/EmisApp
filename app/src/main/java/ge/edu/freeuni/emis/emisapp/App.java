0844e659ffef9538f8415e70fa6ac893393372e5
ggham <ggham12@freeuni.edu.ge>
2015-07-07 17:20:29 +0400

    initial commit

---
+++ app/src/main/java/ge/edu/freeuni/emis/emisapp/App.java
@@ -0,0 +1,17 @@
+package ge.edu.freeuni.emis.emisapp;
+
+import android.app.Application;
+
+/**
+ * Created by giorgi on 7/7/15.
+ */
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    private void mockInit() {
+
+    }
+}
