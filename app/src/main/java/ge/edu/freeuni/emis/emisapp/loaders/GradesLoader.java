6b122205f95f459d28a6ef8bdeb469d5721976ca
gosipov <gosip12@freeuni.edu.ge>
2015-07-14 01:06:17 +0400

    detailed grade json parsing added

--- app/src/main/java/ge/edu/freeuni/emis/emisapp/loaders/GradesLoader.java
+++ app/src/main/java/ge/edu/freeuni/emis/emisapp/loaders/GradesLoader.java
@@ -3,20 +3,30 @@ package ge.edu.freeuni.emis.emisapp.loaders;
 import android.content.Context;
 import android.os.AsyncTask;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
+import ge.edu.freeuni.emis.emisapp.R;
+import ge.edu.freeuni.emis.emisapp.adapters.DetailedGradesListAdapter;
 import ge.edu.freeuni.emis.emisapp.interfaces.GradesLoadingListener;
 import ge.edu.freeuni.emis.emisapp.interfaces.GradesLoadingSubject;
 import ge.edu.freeuni.emis.emisapp.loaders.util.JsoupUtils;
 import ge.edu.freeuni.emis.emisapp.model.*;
 import ge.edu.freeuni.emis.emisapp.model.Class;
+import ge.edu.freeuni.emis.emisapp.model.grading.Category;
 import ge.edu.freeuni.emis.emisapp.model.grading.Grade;
+import ge.edu.freeuni.emis.emisapp.model.grading.SingleDetailedGrade;
 
 public class GradesLoader extends AsyncTask implements GradesLoadingSubject {
 
@@ -55,9 +65,63 @@ public class GradesLoader extends AsyncTask implements GradesLoadingSubject {
         currClass.setNumCredits(Integer.parseInt(elems.get(2).text()));
         currClass.setStudentsGrade(new Grade(elems.get(4).text(),
                 Double.parseDouble(elems.get(3).text())));
-
-
+        String link = elems.get(5).text();
+        if (!link.equals("")) {
+            String dataLid = classHTML.getElementsByTag("tr").first().attr("data-lid");
+            // here would be code getting json files from web
+            // instead mocking:
+            if (dataLid.equals("805") || dataLid.equals("1292") || dataLid.equals("1296")) {
+                try {
+                    JSONObject json = JsoupUtils.getJSONObject(dataLid, context);
+                    parseJSON(json, currClass);
+                }
+                catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         return currClass;
+    }
+
+    private void parseJSON(JSONObject json, Class cl) throws JSONException {
+        if (json.isNull("comp")) return;
+        JSONObject components = json.getJSONObject("comp");
+        JSONObject eval = json.getJSONObject("est").getJSONObject("6076");
+        Iterator<String> compIter = components.keys();
+        while (compIter.hasNext()) {
+            String categoryID = compIter.next();
+            JSONObject currComp = components.getJSONObject(categoryID);
+            JSONObject currCompEval = null;
+            if (eval.has(categoryID)) {
+                currCompEval = eval.getJSONObject(categoryID);
+            }
+            String categoryInd = currComp.getString("name");
+            if (Arrays.asList(context.getResources().getStringArray(R.array.unused_cats))
+                    .contains(categoryInd)) continue;
+            double categoryScore = 0;
+            if (currCompEval != null && currCompEval.get("shedegi")
+                    .getClass().equals(Double.class)) {
+                categoryScore = currCompEval.getDouble("shedegi");
+            }
+            int numSubComps = Integer.parseInt(currComp.getString("count"));
+            JSONArray data = currComp.getJSONArray("data");
+            for (int i = 1; i <= numSubComps; ++i) {
+                JSONObject pair = data.getJSONObject(i-1);
+                double weight = Double.parseDouble(pair.getString("xvedriti_cona"));
+                double maxScore = Double.parseDouble(pair.getString("max_shefaseba"));
+                String gradeNumber = Integer.toString(i);
+                double result = 0;
+                double score = 0;
+                if (currCompEval != null && numSubComps > 1) {
+                    JSONObject currEval = currCompEval.getJSONObject(gradeNumber);
+                    result = currEval.getDouble("shedegi");
+                    score = currEval.getDouble("val");
+                }
+                SingleDetailedGrade detailedGrade = new SingleDetailedGrade(
+                        i, score, maxScore, weight, result);
+                cl.addDetailedGrade(categoryInd, detailedGrade, categoryScore);
+            }
+        }
     }
 
     @Override
