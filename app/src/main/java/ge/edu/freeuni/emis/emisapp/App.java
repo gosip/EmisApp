949506cdd63adfd1125bd4639e6e6f16c65763e1
gosipov <gosip12@freeuni.edu.ge>
2015-07-16 00:41:37 +0400

    app updated to turn on alarm

--- app/src/main/java/ge/edu/freeuni/emis/emisapp/App.java
+++ app/src/main/java/ge/edu/freeuni/emis/emisapp/App.java
@@ -1,17 +1,22 @@
 package ge.edu.freeuni.emis.emisapp;
 
+import android.app.AlarmManager;
 import android.app.Application;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.os.SystemClock;
 import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import ge.edu.freeuni.emis.emisapp.background.RefreshService;
 import ge.edu.freeuni.emis.emisapp.interfaces.AppStateListener;
 import ge.edu.freeuni.emis.emisapp.interfaces.AppStateSubject;
 import ge.edu.freeuni.emis.emisapp.interfaces.GradesLoadingListener;
@@ -39,49 +44,66 @@ public class App extends Application implements
     private static final boolean DEFAULT_NOTIF = true;
     private static final int DEFAULT_NOTIF_PERIOD = 5;
     private static final boolean DEFAULT_REFRESH_ON = true;
+    private static final int DEFAULT_PAUSE_MS = 5 * 60 * 1000;
 
     private List<AppStateListener> listeners;
 
     private List<Semester> semesterList;
     private Student student;
     private List<TranscriptRow> transcript;
-    private UserPreferences userPreferences;
-
     private StudentData internalData;
 
+    private SharedPreferences preferences;
+    private UserPreferences userPreferences;
+
     private boolean networkConnectionAvailable;
+
+    private static AlarmManager am = null;
+    private static PendingIntent pIntent;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        init();
         setDefaultPreferences();
         initUserPreferences();
-        userPreferences.setRefreshPeriod(10);
-        userPreferences.setNotificationsOn(false);
-        storePreferences();
-        initUserPreferences();
-
-        init();
+        if (userPreferences.isRefreshOn())
+            setAlarm(this, userPreferences.getRefreshPeriod());
         initiateInfoLoading();
         if (networkConnectionAvailable)
             initiateLoadingFromWeb();
     }
 
+    public static void setAlarm(Context context, long intervalMillis) {
+        Intent intent = new Intent(context, RefreshService.class);
+        pIntent = PendingIntent.getBroadcast(context, 0, intent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+
+        am = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + DEFAULT_PAUSE_MS,
+                intervalMillis * 1000, pIntent);
+    }
+
+    public void updateAlarmInterval(long intervalMillis) {
+        am.setInexactRepeating(AlarmManager.ELAPSED_REALTIME,
+                SystemClock.elapsedRealtime() + DEFAULT_PAUSE_MS ,
+                intervalMillis * 1000, pIntent);
+    }
+
+    public void turnAlarmOff() {
+        if (am != null) {
+            am.cancel(pIntent);
+        }
+    }
+
     private void setDefaultPreferences() {
-        SharedPreferences preferences = this.getSharedPreferences(
-                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         if (!preferences.contains(getString(R.string.notification_period))) {
-            SharedPreferences.Editor editor = preferences.edit();
-            editor.putBoolean(getString(R.string.notification_pref), DEFAULT_NOTIF);
-            editor.putInt(getString(R.string.notification_period), DEFAULT_NOTIF_PERIOD);
-            editor.putBoolean(getString(R.string.refresh_on), DEFAULT_REFRESH_ON);
-            editor.commit();
+            storePreferences();
         }
     }
 
     private void initUserPreferences() {
-        SharedPreferences preferences = this.getSharedPreferences(
-                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         this.userPreferences = new UserPreferences();
         userPreferences.setRefreshOn(preferences.getBoolean(getString(R.string.refresh_on),
                 DEFAULT_REFRESH_ON));
@@ -93,13 +115,11 @@ public class App extends Application implements
     }
 
     public void storePreferences() {
-        SharedPreferences preferences = this.getSharedPreferences(
-                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = preferences.edit();
         editor.putBoolean(getString(R.string.notification_pref), userPreferences.isNotificationsOn());
         editor.putInt(getString(R.string.notification_period), userPreferences.getRefreshPeriod());
         editor.putBoolean(getString(R.string.refresh_on), userPreferences.isRefreshOn());
-        editor.commit();
+        editor.apply();
     }
 
     private void init() {
@@ -107,6 +127,8 @@ public class App extends Application implements
         semesterList = new ArrayList<>();
         transcript = new ArrayList<>();
         internalData = null;
+        preferences = this.getSharedPreferences(
+                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         // checking network connections
         ConnectivityManager cm = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -152,8 +174,6 @@ public class App extends Application implements
 
     @Override
     public void onPersonalInfoDownloaded(Student studentInfo) {
-        studentInfo.setProfileImg(BitmapFactory.decodeResource(getResources(),
-                R.drawable.profile_avatar_placeholder));
         this.student = studentInfo;
         notifyStudentInfoUpdated(studentInfo);
     }
